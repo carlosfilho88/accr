b3dc7f5ab13ccae8a410fc65e5e654b033daca46
Carlos Filho <carlos@humanasaude.com.br>
2015-07-03 08:32:22 -0300

    Removendo JPA

--- src/main/java/entidades/Usuario.java
+++ src/main/java/entidades/Usuario.java
@@ -3,37 +3,16 @@ package entidades;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="USUARIO")
 public class Usuario implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4306499762696262279L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(name="NOME", length=255, nullable=false)
 	private String nome;
-	@Column(name="CPF", length=11, unique=true)
 	private String cpf;
-	@Column(name="SENHA", length=15, nullable=false)
 	private String senha;
-	@Column(name="DT_NASCIMENTO")
 	private Date dataNascimento;
-	@Column(name="STATUS", columnDefinition = "tinyint default 1")
 	private int status = 1; 
-	@Column(name="CREATED", columnDefinition = "datetime default current_timestamp")
 	private Date created;
-	@Column(name="MODIFIED", columnDefinition = "datetime default current_timestamp")
 	private Date modified;
 	
 	public int getId() {
