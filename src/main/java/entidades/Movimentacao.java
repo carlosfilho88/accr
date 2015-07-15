9b5b752add48bcfcef28521651d890c8e345f358
Airon Wellington Lima dos Santos <aironwellington@gmail.com>
2015-07-14 22:04:47 -0300

    Classe de entidade movimentacao

---
+++ src/main/java/entidades/Movimentacao.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Airon
+ */
+public class Movimentacao implements Serializable{
+    
+    private int id;
+    private int itemId;
+    private int userId;
+    private int status;
+    private Date created;
+    private Date modified;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+    
+    public String toString(){
+        return this.id + "" + this.itemId + this.userId + "" + this.status + "" + this.created + "" + this.modified;
+    }
+    
+}
