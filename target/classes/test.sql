1312f7ff4127210a568f5bfe204d930a8da744c7
Carlos Filho <carlosfilho88@gmail.com>
2015-07-16 00:00:49 -0300

    Novo SQL

--- target/classes/test.sql
+++ target/classes/test.sql
@@ -16,6 +16,45 @@ CREATE DATABASE IF NOT EXISTS `libsystem` /*!40100 DEFAULT CHARACTER SET utf8 CO
 USE `libsystem`;
 
 
+-- Dumping structure for table libsystem.item
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `edicao` int(10) unsigned DEFAULT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table libsystem.item: ~1 rows (approximately)
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` (`id`, `descricao`, `quantidade`, `ano`, `edicao`, `autor`) VALUES
+	(1, 'Java Como Programar', 10, 2010, 8, 'Paul Deitel');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+
+
+-- Dumping structure for table libsystem.movimentacoes
+DROP TABLE IF EXISTS `movimentacoes`;
+CREATE TABLE IF NOT EXISTS `movimentacoes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `tipo_movimentacao` char(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  `user_id_modified` int(10) unsigned NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table libsystem.movimentacoes: ~1 rows (approximately)
+/*!40000 ALTER TABLE `movimentacoes` DISABLE KEYS */;
+INSERT INTO `movimentacoes` (`id`, `item_id`, `tipo_movimentacao`, `created`, `modified`, `user_id_modified`, `status`) VALUES
+	(1, 1, 'E', '2015-07-15 00:48:21', NULL, 1, 1);
+/*!40000 ALTER TABLE `movimentacoes` ENABLE KEYS */;
+
+
 -- Dumping structure for table libsystem.perfil
 DROP TABLE IF EXISTS `perfil`;
 CREATE TABLE IF NOT EXISTS `perfil` (
@@ -46,12 +85,13 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` tinyint(2) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Dumping data for table libsystem.usuario: ~1 rows (approximately)
+-- Dumping data for table libsystem.usuario: ~2 rows (approximately)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 INSERT INTO `usuario` (`id`, `perfil_id`, `cpf`, `nome`, `senha`, `data_nascimento`, `created`, `modified`, `status`) VALUES
-	(1, 1, '03451889340', 'Carlos Filho', '123456', '1988-10-11', '2015-07-12 16:06:25', NULL, 1);
+	(1, 2, '03451889340', 'Carlos Filho', '123456', '1988-10-11', '2015-07-12 16:06:25', NULL, 1),
+	(2, 1, '02647064393', 'Airon Lima', '123456', '1988-04-18', '2015-07-15 23:57:07', '2015-07-15 23:57:07', 1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
